73da2a5f97370264dfd698071353422e19e9ce4f
hsauro <hsauro@u.washington.edu>
2013-12-20 12:31:50 -0800

    Attempt to add events
    Requires fixes to OnProgress in the C API first to work

--- plugins/wrappers/Python/rrPlugins.py
+++ plugins/wrappers/Python/rrPlugins.py
@@ -1,48 +1,76 @@
 ##@Module rrPlugins_CAPI
 
-import rrPlugins_CAPI as rrp 
+import rrPlugins_CAPI as rrp
 import matplotlib.pyplot as plt
 
 __version__ = "0.5.01"
 
 class DataSeries(object):
-    
+
     _data = 0
-    
+
     def __init__ (self, handle):
         self._data = handle
-        
+
     def __del__ (self):
-        if (self._data != 0): 
-            try:            
+        if (self._data != 0):
+            try:
                rrp.freeRoadRunnerData (self._data)
             except:
                 print "Exception"
             self._data = 0
-    
+
     def __getHandle (self):
         return self._data
-        
+
     def __AsNumpy (self):
         return rrp.getNumpyData (self._data)
 
     data = property (__getHandle)
     AsNumpy = property (__AsNumpy)
-    
+
 
 # ------------------------------------------------------------------------
-    
+
+class Event(object):
+
+    def __init__(self):
+        self.handlers = []
+
+    def add(self, handler):
+        self.handlers.append(handler)
+        return self
+
+    def remove(self, handler):
+        self.handlers.remove(handler)
+        return self
+
+    def fire(self, sender, earg=None):
+        for handler in self.handlers:
+            handler(sender, earg)
+
+    __iadd__ = add
+    __isub__ = remove
+    __call__ = fire
+
+
+
+# ------------------------------------------------------------------------
+
 _pluginManager = rrp.createPluginManager()
 _pluginsAlreadyLoaded = False
-    
+
 class Plugin:
+
+    _OnProgressList = Event()
+    _onProgressEvent = 0
 
     def __init__(self, pluginName):
         self.pluginName = pluginName
         self.plugin = rrp.loadPlugin (_pluginManager, pluginName)
-               
+
     def setParameter(self, name, value):
-        if (isinstance (value, DataSeries)):        
+        if (isinstance (value, DataSeries)):
            rrp.setPluginParameter (self.plugin, name, value.data)
         else:
            handle  = rrp.getPluginParameter(self.plugin, name);
@@ -54,56 +82,69 @@ class Plugin:
                  if not isinstance(value[0], str):
                      raise TypeError("Expecting parameter name in first element of list")
                  if (not isinstance(value[1], float)) and (isinstance(value[1], int)):
-                     raise TypeError("Expecting floating value in second element of list")               
+                     raise TypeError("Expecting floating value in second element of list")
                  para1 = rrp.createParameter(value[0], "double", "", value[1])
                  rrp.addParameterToList (handle, para1)
               else:
                  raise  TypeError ("Expecting a list in setParameter")
            else:
               rrp.setPluginParameter (self.plugin, name, value)
-        
+
     def getParameter (self, name):
         handle = rrp.getPluginParameter (self.plugin, name)
-        value = rrp.getParameter (handle)        
+        value = rrp.getParameter (handle)
         if (rrp.getParameterType(handle) == "roadRunnerData"):
             return DataSeries (value)
-        else:        
-           return value    
-          
-    def listOfParameters ():          
-        return ['x', 'y']
-        
+        else:
+           return value
+
+    def listOfParameters (self):
+        nameList = rrp.getListOfPluginParameterNames (self.plugin)
+        aList = []
+        for i in range (0, len (nameList)):
+            name = nameList[i]
+            handle = rrp.getPluginParameter(self.plugin, nameList[i])
+            hint = rrp.getParameterHint(handle)
+            aList.append ([name, hint])
+        return aList
+
     def loadDataSeriesAsNumPy (self, fileName):
         rrDataHandle = rrp.createRoadRunnerDataFromFile (fileName)
         return rrp.getNumpyData (rrDataHandle)
-        
+
     def loadDataSeries (self, fileName):
         handle = rrp.createRoadRunnerDataFromFile (fileName)
         return DataSeries(handle)
-        
+
+    def OnProgress (self, f):
+        global _onProgressEvent
+
+        _onProgressEvent =  rrp.NotifyIntIntEvent(f)
+        rrp.assignOnProgressEvent(self.plugin, _onProgressEvent)
+
     def execute (self):
         return rrp.executePlugin (self.plugin)
 
-    def executeEx (self):        
+    def executeEx (self):
         return rrp.executePluginEx (self.plugin)
-        
+
     def plotTimeSeriesHandle (self, dataSeries):
-        if (isinstance (dataSeries, DataSeries)):        
+        if (isinstance (dataSeries, DataSeries)):
            hdr = rrp.getRoadRunnerDataColumnHeader(dataSeries.data)
            npData = rrp.getNumpyData(dataSeries.data)
            rrp.plotRoadRunnerData(npData, hdr)
         else:
            raise TypeError ("Expecting DataSeries type")
-           
+
     def readAllText(self, fName):
         file = open(fName, 'r')
         str = file.read()
         file.close()
         return str
-        
+
     def loadPlugins(self):
         rrp.loadPlugins (self.pluginsManager)
-        
+
     @staticmethod
     def listOfPlugins():
         global _pluginsAlreadyLoaded
@@ -111,24 +152,24 @@ class Plugin:
         if not _pluginsAlreadyLoaded:
            rrp.loadPlugins (_pluginManager)
            _pluginsAlreadyLoaded = True
-           
+
         aList = []
-        names = rrp.getPluginLibraryNames (_pluginManager) 
+        names = rrp.getPluginLibraryNames (_pluginManager)
         n = rrp.getNumberOfPlugins (_pluginManager)
-        for i in range (0, n): 
-            handle = rrp.getPlugin(_pluginManager, names[i])  
-            info = rrp.getPluginInfo (handle)  
-            info = info.split ("\n")        
-            #name = rrp.getPluginName(handle)
+        # This is a hack to get round thelack of metadata in the plugin
+        # Will be resolved in next revision of library
+        for i in range (0, n):
+            handle = rrp.getPlugin(_pluginManager, names[i])
+            info = rrp.getPluginInfo (handle)
+            info = info.split ("\n")
             hint = info[2]
             hint = hint.replace("Category......................", "")
             aList.append ([names[i], hint])
-        return aList        
-        
-       
+        return aList
+
 def extractColumn (data, index):
     return data[:,index]
-    
+
 def plot (data, myColor="red", myLinestyle="None", myMarker="None", myLabel=""):
     columns = data.shape[1]
     for i in range(columns-1):
@@ -136,28 +177,26 @@ def plot (data, myColor="red", myLinestyle="None", myMarker="None", myLabel=""):
         plt.setp (p, color=myColor, marker=myMarker, linestyle = myLinestyle, linewidth=1, label=myLabel)
         plt.legend(bbox_to_anchor=(1.05, 1), loc=1, borderaxespad=0.)
         return p
-         
+
 if __name__=='__main__':
-    
+
+    def sayHello (arg1, arg2):
+        print "Hello"
+
     print "Starting Test"
-    
-    rrp.loadPlugins (_pluginManager)
-    print rrp.getPluginLibraryNames (_pluginManager)
-    p = rrp.getPlugin(_pluginManager, "rrp_add_noise")    
-    #print p    
-    info = rrp.getPluginInfo (p)    
-    print info
-    info = info.split ("\n")
-    #print info
-    print info[0].replace (".", "")
-#    p = Plugin ("rrp_add_noise")
+
+    p = Plugin ("rrp_add_noise")
+    pl = p.listOfParameters()
+    for item in pl:
+        print item
+
+    p.OnProgress (sayHello)
 #    p.setParameter ("Sigma", 0.0001)
-#    
-#    series = p.loadDataSeries (".\\Examples\\testData.dat") 
+#
+#    series = p.loadDataSeries (".\\Examples\\testData.dat")
 #    p.plotTimeSeriesHandle (series)
 #    p.setParameter ("InputData", series)
 #    p.execute()
 #    p.plotTimeSeriesHandle (series)
-    
+
     print "Test Finished"
-        
