faacb35feadf1cb9d6063bc4747762bd79054c33
Totte Karlsson <tottek@gmail.com>
2013-12-21 00:39:59 -0800

    Updated in the short lm demo
    changed path to be relative do models, in a proper install

--- plugins/wrappers/Python/Examples/rrLevenbergMarquardtShortVersion.py
+++ plugins/wrappers/Python/Examples/rrLevenbergMarquardtShortVersion.py
@@ -1,11 +1,18 @@
+from rrPlugins_CAPI import *
 import rrPlugins as rrp
 
-lm = rrp.Plugins ("rrp_lm")
+lm = rrp.Plugin ("rrp_lm")
+##========== LMFIT EVENT FUNCTIONS ==================
+def pluginIsProgressing(progressMessage):
+    print progressMessage
+progressEvent =  NotifyStringEvent(pluginIsProgressing)
+assignOnProgressEvent(lm.plugin, progressEvent)
+##===================================================
 
 experimentalData = lm.loadDataSeries ("testData.dat")
 
 lm.setParameter ("ExperimentalData", experimentalData)
-lm.setParameter ("SBML", lm.readAllText("sbml_test_0001.xml"))
+lm.setParameter ("SBML", lm.readAllText("../../models/sbml_test_0001.xml"))
 
 # Add the parameters that we're going to fit and the initial value
 lm.setParameter ("InputParameterList", ["k1", 0.2])
@@ -15,6 +22,11 @@ lm.setParameter("ExperimentalDataSelectionList", "[S1] [S2]")
 
 # Execute lmfit plugin
 res = lm.execute()
+
+## ----------- Uncomment the following to test in a thread
+##res = lm.executeEx(True)
+##while isPluginWorking(lm.plugin):
+##    print 'Working'
 
 # Get the experimental data as a numpy array
 experimentalData = experimentalData.AsNumpy
@@ -29,6 +41,9 @@ rrp.plot (residuals[:,[0,1]], myColor="blue", myLinestyle="None", myMarker="x", 
 rrp.plot (residuals[:,[0,2]], myColor="red", myLinestyle="None", myMarker="x", myLabel="S2 Residual")
 rrp.plot (experimentalData[:,[0,1]], myColor="red", myLinestyle="", myMarker="*", myLabel="S1 Data")
 rrp.plot (experimentalData[:,[0,2]], myColor="blue", myLinestyle="", myMarker="*", myLabel="S2 Data")
+rrp.plt.show()
+
+print getPluginResult(lm.plugin)
 
 
 
