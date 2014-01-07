6c87d88fe0f5b4cbebb563ce2a1e2ef644ef8f4b
hsauro <hsauro@u.washington.edu>
2014-01-06 22:52:53 -0800

    Updated comments in rrCreateNoisyDataShort.py

--- plugins/wrappers/Python/Examples/rrCreateNoisyDataShort.py
+++ plugins/wrappers/Python/Examples/rrCreateNoisyDataShort.py
@@ -13,16 +13,19 @@ rr.load("sbml_test_0001.xml")
 # Generate data
 rr.simulate(0, 10, 511) # Want 512 points
 
-# The plugin will need a handle to the underlying roadrunner data
+# Get the dataseries from roadrunner
 d = rrp.getRoadRunnerData (rr)
 
+# Assign the dataseries to the plugin inputdata
 noisePlugin.InputData = d
 
-# Get parameter for the 'size' of the noise
+# Set parameter for the 'size' of the noise
 noisePlugin.Sigma = 3.e-5
 
+# Add the noise
 noisePlugin.execute ()
 
+# Get the data to plot
 numpydata = noisePlugin.InputData.AsNumpy;
 
 rrp.plot (numpydata[:,[0,1]], myColor="blue", myLinestyle="-", myMarker="", myLabel="S1 Fitted")
